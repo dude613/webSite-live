15f8405e9d0cb08445ddac23c7f36ee6ba6b61c3
dude613 <liamswift2014@gmail.com>
2020-02-06 10:49:58 -0500

    contact

--- HTML/contact.php
+++ HTML/contact.php
@@ -23,7 +23,7 @@
     ?>
     
     <!-- Hero Start -->
-    <section class="bg-half bg-light" style="background: url('images/contact-detail.jpg') center center;">
+    <section class="bg-half bg-light" style="background: center center;">
         <div class="home-center">
             <div class="home-desc-center">
                 <div class="container">
@@ -117,7 +117,7 @@
                         </div>
                         <div class="content mt-3">
                             <h4 class="title font-weight-bold">Phone</h4>
-                            <p class="text-muted">Start working with FlexBot that can provide everything</p>
+                            <p class="text-muted">  </p>
                             <a href="tel:+16464946771" class="text-primary">+1 646 494 6771</a>
                         </div>  
                     </div>
@@ -130,7 +130,7 @@
                         </div>
                         <div class="content mt-3">
                             <h4 class="title font-weight-bold">Email</h4>
-                            <p class="text-muted">Start working with FlexBot that can provide everything</p>
+                            <p class="text-muted">  </p>
                             <a href="mailto:info@bflexd.com" class="text-primary">info@bflexd.com</a>
                         </div>  
                     </div>
@@ -143,7 +143,7 @@
                         </div>
                         <div class="content mt-3">
                             <h4 class="title font-weight-bold">Location</h4>
-                            <p class="text-muted">368 9th Ave, <br>New York, NY 10001</p>
+                            <p class="text-muted">  </p>
                             <a href="https://maps.google.com/maps?q=368%209th%20Ave%2C%20New%20York%2C%20NY%2010001&t=&z=13&ie=UTF8&iwloc=&output=embed" class="video-play-icon h6 text-primary">View on Google map</a>
                         </div>  
                     </div>
@@ -154,88 +154,86 @@
     <!-- End contact -->
 
     <!-- Footer Start -->
-    <footer class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-12 mb-0 mb-md-4 pb-0 pb-md-2">
-                    <a class="logo-footer" href="#">FlexBot<span class="text-primary">.</span></a>
-                    <p class="mt-4">Start working with FlexBot that can provide everything you need to generate awareness, drive traffic, connect.</p>
-                    <ul class="list-unstyled social-icon social mb-0 mt-4">
-                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
-                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
-                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
-                    </ul><!--end icon-->
-                </div><!--end col-->
-                
-                <div class="col-lg-2 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
-                    <h4 class="text-light footer-head">Company</h4>
-                    <ul class="list-unstyled footer-list mt-4">
-                        <li><a href="page-aboutus.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> About us</a></li>
-                        <li><a href="page-services.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Services</a></li>
-                        <li><a href="page-team.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Team</a></li>
-                        <li><a href="page-pricing.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Pricing</a></li>
-                        <li><a href="page-work.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Project</a></li>
-                        <li><a href="page-jobs.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Careers</a></li>
-                        <li><a href="page-blog.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Blog</a></li>
-                        <li><a href="page-cover-login.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Login</a></li>
-                    </ul>
-                </div><!--end col-->
-                
-                <div class="col-lg-3 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
-                    <h4 class="text-light footer-head">Usefull Links</h4>
-                    <ul class="list-unstyled footer-list mt-4">
-                        <li><a href="page-terms.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Terms of Services</a></li>
-                        <li><a href="page-privacy.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Privacy Policy</a></li>
-                        <li><a href="documentation.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Documentation</a></li>
-                        <li><a href="changelog.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Changelog</a></li>
-                        <li><a href="components.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Components</a></li>
-                    </ul>
-                </div><!--end col-->
+            <footer class="footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 col-12 mb-0 mb-md-4 pb-0 pb-md-2">
+                            <a class="logo-footer" href="#">FlexBot<span class="text-primary">.</span></a>
+                            <p class="mt-4">Build your automation infastructure with flexBot's robotic process automations & be assured your organization is operating at peak efficiency. </p>
+                            <ul class="list-unstyled social-icon social mb-0 mt-4">
+                                <li class="list-inline-item"><a href="mailto:info@bflexd.com" class="rounded"><i class="mdi mdi-email" title="Email"></i></a></li>
+                                <li class="list-inline-item"><a href="https://www.linkedin.com/company/flexbots/" class="rounded"><i class="mdi mdi-linkedin" title="Linkedin"></i></a></li>
+                                <li class="list-inline-item"><a href="https://twitter.com/bflexd" class="rounded"><i class="mdi mdi-twitter" title="Twitter"></i></a></li>
+                                <li class="list-inline-item"><a href="SMS:+16464946771;?&body=Hi,%20I%20would%20like%20to%20learn%20more" class="rounded"><i class="mdi mdi-phone" title="Phone"></i></a></li>
+                            
+                            </ul><!--end icon-->
+                        </div><!--end col-->
+                        
+                        <div class="col-lg-2 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                            <h4 class="text-light footer-head">Company</h4>
+                            <ul class="list-unstyled footer-list mt-4">
+                                <li><a href="page-aboutus.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> About</a></li>
+                                <li><a href="page-aboutus.html#team" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Team</a></li>
+                                <li><a href="page-pricing.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Pricing</a></li>
+                                <li><a href="page-jobs.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Careers</a></li>
+                                <li><a href="page-cover-login.html" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Login</a></li>
+                            </ul>
+                        </div><!--end col-->
+                        
+                        <div class="col-lg-3 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                            <h4 class="text-light footer-head">Useful Links</h4>
+                            <ul class="list-unstyled footer-list mt-4">
+                                <li><a href="terms.php" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Terms</a></li>
+                                <li><a href="privacy.php" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Privacy</a></li>
+                                <li><a href="compliance.php" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Compliance</a></li>
+                                <li><a href="partners.php" class="text-foot"><i class="mdi mdi-chevron-right mr-1"></i> Partners</a></li>
+                            </ul>
+                        </div><!--end col-->
 
-                <div class="col-lg-3 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
-                    <h4 class="text-light footer-head">Newletter</h4>
-                    <p class="mt-4">Sign up and receive the latest tips via email.</p>
-                    <form>
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="foot-subscribe form-group position-relative">
-                                    <label>Write your email <span class="text-danger">*</span></label>
-                                    <i class="mdi mdi-email ml-3 icons"></i>
-                                    <input type="email" name="email" id="emailsubscribe" class="form-control pl-5 rounded" placeholder="Your email : " required>
+                        <div class="col-lg-3 col-md-4 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                            <h4 class="text-light footer-head">Bot Updates</h4>
+                            <p class="mt-4">Enter your work email to recieve the latest updates in automation.</p>
+                            <form>
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="foot-subscribe form-group position-relative">
+                                            <label>Enter your email <span class="text-danger">*</span></label>
+                                            <i class="mdi mdi-email ml-3 icons"></i>
+                                            <input type="email" name="email" id="emailsubscribe" class="form-control pl-5 rounded" placeholder="Email : " required>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <input type="submit" id="submitsubscribe" name="send" class="btn btn-primary rounded w-100" value="Subscribe">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-lg-12">
-                                <input type="submit" id="submitsubscribe" name="send" class="btn btn-primary w-100" value="Subscribe">
+                            </form>
+                        </div><!--end col-->
+                    </div><!--end row-->
+                </div><!--end container-->
+            </footer><!--end footer-->
+            <hr>
+            <footer class="footer footer-bar">
+                <div class="container text-center">
+                    <div class="row align-items-center">
+                        <div class="col-sm-6">
+                            <div class="text-sm-left">
+                                <p class="mb-0">© 2020 FlexBot. Proudly Powered by <a href="https://ayamllc.com/" target="_blank" class="text-success">The Ayam Group, LLC</a>.</p>
                             </div>
                         </div>
-                    </form>
-                </div><!--end col-->
-            </div><!--end row-->
-        </div><!--end container-->
-    </footer><!--end footer-->
-    <hr>
-    <footer class="footer footer-bar">
-        <div class="container text-center">
-            <div class="row align-items-center">
-                <div class="col-sm-6">
-                    <div class="text-sm-left">
-                        <p class="mb-0">© 2019-20 FlexBot. Design with <i class="mdi mdi-heart text-danger"></i> by <a href="http://www.shreethemes.in/" target="_blank" class="text-success">Shreethemes</a>.</p>
-                    </div>
-                </div>
 
-                <div class="col-sm-6 mt-4 mt-sm-0 pt-2 pt-sm-0">
-                    <ul class="list-unstyled payment-cards text-sm-right mb-0">
-                        <li class="list-inline-item"><a href="javascript:void(0)"><img src="images/payments/american-ex.png" title="American Express" alt=""></a></li>
-                        <li class="list-inline-item"><a href="javascript:void(0)"><img src="images/payments/discover.png" title="Discover" alt=""></a></li>
-                        <li class="list-inline-item"><a href="javascript:void(0)"><img src="images/payments/master-card.png" title="Master Card" alt=""></a></li>
-                        <li class="list-inline-item"><a href="javascript:void(0)"><img src="images/payments/paypal.png" title="Paypal" alt=""></a></li>
-                        <li class="list-inline-item"><a href="javascript:void(0)"><img src="images/payments/visa.png" title="Visa" alt=""></a></li>
-                    </ul>
-                </div>
-            </div>
-        </div><!--end container-->
-    </footer><!--end footer-->
-    <!-- Footer End -->
+                        <div class="col-sm-6 mt-4 mt-sm-0 pt-2 pt-sm-0">
+                            <ul class="list-unstyled payment-cards text-sm-right mb-0">
+                                <li class="list-inline-item"><img src="images/payments/american-ex.png" title="American Express" alt=""></a></li>
+                                <li class="list-inline-item"><img src="images/payments/discover.png" title="Discover" alt=""></a></li>
+                                <li class="list-inline-item"><img src="images/payments/master-card.png" title="Master Card" alt=""></a></li>
+                                <li class="list-inline-item"><img src="images/payments/paypal.png" title="Paypal" alt=""></a></li>
+                                <li class="list-inline-item"><img src="images/payments/visa.png" title="Visa" alt=""></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div><!--end container-->
+            </footer><!--end footer-->
+            <!-- Footer End -->
 
     <!-- Back to top -->
     <a href="#" class="back-to-top rounded text-center" id="back-to-top"> 
