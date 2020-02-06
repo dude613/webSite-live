375c99a7d7e337b8acaa33ebf1c5e50546d99471
dude613 <liamswift2014@gmail.com>
2020-02-05 22:47:49 -0500

    keywords

--- HTML/head.php
+++ HTML/head.php
@@ -2,9 +2,9 @@
     <meta charset="utf-8" />
     <title>FlexBot - Robotic Process Automation for Businesses</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Premium Bootstrap 4 Landing Page Template" />
-    <meta name="keywords" content="bootstrap 4, premium, marketing, multipurpose" />
-    <meta content="Shreethemes" name="author" />
+    <meta name="description" content="Robotic Process Automation Digital Robot Solutions" />
+    <meta name="keywords" content="process automation, rpa, digital robots" />
+    <meta content="Robotic Process Automation" name="FlexBot" />
     <!-- favicon -->
     <link rel="shortcut icon" href="images/favicon.ico">
     <!-- Bootstrap -->
